f18c732d7616991dda46bf732dc971ac4f520e30
Kishor Mahmud <mahmudkishor9@gmail.com>
2026-05-14 01:28:55 +0600

    deployment related change

--- frontend/app/(auth)/forgot-password/page.tsx
+++ frontend/app/(auth)/forgot-password/page.tsx
@@ -1,5 +1,8 @@
 import ForgotPasswordForm from "@/components/auth/ForgotPasswordForm";
+import { Suspense } from "react";
 
 export default function ForgotPasswordPage() {
-    return <ForgotPasswordForm />;
+    <Suspense fallback={<div>Loading...</div>}>
+        <ForgotPasswordForm />
+    </Suspense>
 }
